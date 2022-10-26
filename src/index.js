aa0da285754102e41ebba4e61397b6bcd02f3af2
qwerty0121 <sh1ng0718o@gmail.com>
2022-10-26 22:06:49 +0900

    [update] コメントを追加

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import CounterButton from './components/CounterButton.vue'
+
+// npmパッケージとして公開するVueコンポーネントをエクスポートする
 export { CounterButton }
--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,18 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     lib: {
+      // エントリーポイントとなるファイルを指定
       entry: resolve(__dirname, 'src/index.js'),
+      // HTMLから<script>タグを用いてnpmパッケージをインストールした際、パッケージ内のVueコンポーネントがこの名前のグローバル変数に設定される
       name: 'sample-vue-component-npm-package',
+      // ビルド結果として出力されるパッケージファイルの名前(を返却値とする関数)
       fileName: (format) => `sample-vue-component-npm-package.${format}.js`
     },
     rollupOptions: {
+      // バンドル対象外とする依存ライブラリを指定する
       external: 'vue',
       output: {
+        // バンドル対象外とした依存ライブラリのためにUMDビルドで使用するグローバル変数を提供する
         globals: {
           vue: 'Vue'
         }
